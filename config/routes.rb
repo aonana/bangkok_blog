095003a426e1e9d39d33196a7f8e7de794e9e950
EC2 Default User <ec2-user@ip-172-31-35-182.ap-southeast-1.compute.internal>
2019-11-27 09:28:37 +0000

    トップページにアクセスした場合に動くアクションを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'blogs#top'
+  root 'blogs#index'
  resources :blogs do
     resources :comments, only: [:create]
   end
