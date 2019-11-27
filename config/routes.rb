2b50062175177776c3c7561accdd11b38237cc5d
EC2 Default User <ec2-user@ip-172-31-35-182.ap-southeast-1.compute.internal>
2019-11-27 09:56:34 +0000

    最初に開いたらトップページに移動

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'blogs#index'
+  root 'blogs#top'
  resources :blogs do
     resources :comments, only: [:create]
   end
